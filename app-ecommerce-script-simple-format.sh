72c128ec22daf07b8ef11ddd34c19d9de29cc3b6
SyedYakhub <syedyakhub9966@gmail.com>
2023-08-05 20:25:09 +0530

    update script file

--- app-ecommerce-script-simple-format.sh
+++ app-ecommerce-script-simple-format.sh
@@ -11,6 +11,9 @@ sudo systemctl enable mariadb
 sudo firewall-cmd --permanent --zone=public --add-port=3306/tcp
 sudo firewall-cmd --reload
 
+#Enter mysql command to intercat with mariadb cli
+# then you can enter the below command manually except cat command
+
 sudo cat > configure-db.sh <<-EOF
 
 CREATE DATABASE ecomdb;
@@ -22,6 +25,12 @@ EOF
 
 sudo mysql < configure-db.sh
 
+#After adding the above lines you can verify by entering command -> show database;
+#you can also login with the credentials using -> mariadb -u ecomuser -p by entering password as ecompassword
+
+#you can also add the below lines manually by loggin using -> mariadb -u ecomuser -p -D ecomdb and enter password
+#then use cat > db-load-script.sql and add the lines followed by mariadb < db-load-script.sql
+
 sudo cat > db-load-script.sql <<-EOF
 USE ecomdb;
 CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name varchar(255) default NULL,Price varchar(255) default NULL, ImageUrl varchar(255) default NULL,PRIMARY KEY (id)) AUTO_INCREMENT=1;
@@ -29,6 +38,9 @@ CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name var
 INSERT INTO products (Name,Price,ImageUrl) VALUES ("Laptop","100","c-1.png"),("Drone","200","c-2.png"),("VR","300","c-3.png"),("Tablet","50","c-5.png"),("Watch","90","c-6.png"),("Phone Covers","20","c-7.png"),("Phone","80","c-8.png"),("Laptop","150","c-4.png");
 
 EOF
+
+#you can login to ecomdb database with the creds using -> mariadb -u ecomuser -p -D ecomdb
+#then enter this line to view the products -> select * from products;
 
 
 sudo mysql < db-load-script.sql
@@ -49,3 +61,37 @@ git clone https://github.com/kodekloudhub/learning-app-ecommerce.git /var/www/ht
 
 
 sudo sed -i 's/172.20.1.101/localhost/g' /var/www/html/index.php
+
+
+
+
+
+# The provided commands are SQL statements executed in the MariaDB database management system. 
+# Let's break down each command and explain its purpose:
+
+# 1. `CREATE DATABASE ecomdb;`
+
+#    This command creates a new database named `ecomdb`. A database is a container for tables, views, 
+#    and other database objects that store and manage data.
+
+# 2. `CREATE USER 'ecomuser'@'localhost' IDENTIFIED BY 'ecompassword';`
+
+#    This command creates a new user named `ecomuser` with the password `ecompassword`. 
+#    The user is created with access restricted to connections from the same host (`localhost`). 
+#    The `IDENTIFIED BY` clause sets the user's password.
+
+# 3. `GRANT ALL PRIVILEGES ON *.* TO 'ecomuser'@'localhost';`
+
+#    This command grants all privileges (full access) on all databases and tables (`*.*`) to the user `ecomuser` 
+#    when connecting from `localhost`. The user is granted the ability to perform various operations, such as creating, 
+#    modifying, and deleting tables and data.
+
+# 4. `FLUSH PRIVILEGES;`
+
+#    This command refreshes the MariaDB privilege cache to apply the changes made by the previous `GRANT` statement immediately. 
+#    After running `FLUSH PRIVILEGES`, the new user (`ecomuser`) will have the granted privileges in effect.
+
+# In summary, these commands set up a new database named `ecomdb`, create a user named `ecomuser` with a password, 
+# grant the user full privileges on all databases and tables when connecting from `localhost`, 
+# and then flush the privileges to apply the changes immediately. This is a basic setup for a database 
+# and user that can be used by an application to interact with the MariaDB database.
